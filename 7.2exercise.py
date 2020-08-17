f6c9f93d284b4d00426c9b3f788d66a8d348658e
Göran Paues <goran.paues@tradedoubler.com>
2020-08-17 21:36:07 +0200

    Assignment 7.2

---
+++ 7.2exercise.py
@@ -0,0 +1,16 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+
+number_of_spam_confidence = 0
+total_spam_value = 0
+
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    else:
+        total_spam_value += float(line[line.find(':')+1:].strip())
+        number_of_spam_confidence += 1
+
+if number_of_spam_confidence > 0:
+    avg_spam_confidence = total_spam_value / number_of_spam_confidence
+    print('Average spam confidence:',avg_spam_confidence)
